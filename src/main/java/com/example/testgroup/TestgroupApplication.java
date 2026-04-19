15f9bd667dff05afa06bef4fea1d317c1201b319
gon <gjgudrhs@naver.com>
2026-04-19 19:21:57 +0900

    first commit

---
+++ src/main/java/com/example/testgroup/TestgroupApplication.java
@@ -0,0 +1,13 @@
+package com.example.testgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestgroupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testgroup/TestgroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
